a9328dcf68ff693a087e170367332b878a376292
lizicheng <lizicheng@gridbear.cn>
2025-04-16 19:30:41 +0800

    变量

---
+++ hello_world.c
@@ -0,0 +1,17 @@
+//
+// Created by ??? on 2025/4/15.
+//
+
+# include "stdio.h"
+
+
+int main(){
+    printf("Hello World!\n");
+    printf("我的名字：%s\n年龄:%d\n体重：%f\n","lzc",25,70.2);
+    int a=10,b=20;
+    printf("%d\n",a+b);
+    a+=5;
+    printf("%d\n",a);
+
+    return 0;
+}
